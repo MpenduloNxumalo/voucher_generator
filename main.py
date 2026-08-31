30e8c58fb04536f7d91ceb0d7197f28c37c5b068
MpenduloNxumalo <mpendulonxumalo67@gmail.com>
2026-08-31 12:46:31 +0200

    Create DEBUG variable that indicates if we in debug more

--- main.py
+++ main.py
@@ -5,3 +5,6 @@ app = FlaskApp(__name__, specification_dir=".")
 app.add_api("api/vouchers_api.yaml")
 BASE_DIR = Path(__file__).resolve().parent
 DEBUG = True
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
