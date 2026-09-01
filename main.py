d9994325536f3feeb4e13b631c6ee3b10515b750
MpenduloNxumalo <mpendulonxumalo67@gmail.com>
2026-09-01 10:15:20 +0200

    Configure DEBUG variable to False

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from pathlib import Path
 app = FlaskApp(__name__, specification_dir=".")
 app.add_api("api/vouchers_api.yaml")
 BASE_DIR = Path(__file__).resolve().parent
-DEBUG = True
+DEBUG = False
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
